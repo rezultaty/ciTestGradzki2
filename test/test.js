b75d26005d327b22b12b97eee19ae6dcfb4b2692
marcingradzki <marcingradzk@gmail.com>
2016-02-02 19:17:02 +0000

    Test2

--- test/test.js
+++ test/test.js
@@ -6,3 +6,11 @@ assert.equal(discountCalculator.discount instanceof Function, true,
 "typeof discount: " + typeof discountCalculator.discount);
 });
 });
+
+describe("discount function", function(){
+it("discount should return 0 with no args ", function(){
+var discount = discountCalculator.discount();
+assert.equal(discount, 0, discount + "");
+});
+});
+
