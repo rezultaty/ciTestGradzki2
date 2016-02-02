0e27b43fa3b585afd2d5afb4888fbb58a339ee1d
marcingradzki <marcingradzk@gmail.com>
2016-02-02 19:20:55 +0000

    Test3

--- test/test.js
+++ test/test.js
@@ -1,11 +1,3 @@
-var assert = require("assert");
-var discountCalculator = require("../discount");
-describe("discount Calculator", function(){
-it("should have discount function", function(){
-assert.equal(discountCalculator.discount instanceof Function, true,
-"typeof discount: " + typeof discountCalculator.discount);
-});
-});
 
 describe("discount function", function(){
 it("discount should return 0 with no args ", function(){
